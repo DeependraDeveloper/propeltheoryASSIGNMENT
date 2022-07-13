ed3344264e29fa0618558b8f425e96b8191924e4
deependra <deependrabahadurr@gmail.com>
2022-07-13 15:42:31 +0530

    fourthdfeature

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,6 +4,7 @@ const profileController = require("../controllers/profileController");
 
 router.post("/profile", profileController.createProfile);
 router.get("/profile/:id", profileController.getProfile);
-router.patch("/profile/:id",profileController.updateProdfile);
-router.delete("/profile/:id",profileController.deleteProfile);
+router.patch("/profile/:id", profileController.updateProdfile);
+router.delete("/profile/:id", profileController.deleteProfile);
+
 module.exports = router;
